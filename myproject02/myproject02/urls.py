eb8a420be5c804c7e11586eba390890ca2a10362
cactusvibeyjy <108244886+cactusvibeyjy@users.noreply.github.com>
2022-10-27 16:34:46 +0900

    Add files via upload
    댓글 기능 + 회원가입 및 로그인 / 로그아웃 기능 구현

--- myproject02/myproject02/urls.py
+++ myproject02/myproject02/urls.py
@@ -19,6 +19,21 @@ from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', views.base),
+    path('write_form/', views.write_form),
 
-    path('', views.index)
+    path('insert/', views.insert),
+    path('list/', views.list),
+    path('list_page/', views.list_page),
+    path('download_count/', views.download_count),
+    path('download/', views.download),
+    path('detail_id/', views.detail_id),
+
+    path('detail/<int:board_id>/', views.detail),
+    path('update_form/<int:board_id>/', views.update_form),
+    path('update/', views.update),
+    path('delete/<int:board_id>/', views.delete),
+    
+    path('comment_insert/', views.comment_insert),
+
 ]
